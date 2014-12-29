a6ad873d4d5236e78b80d6670b0f99ae1c70d1a8
Iñaki Torres <wyrmxd@gmail.com>
2014-12-29 10:40:19 +0000

    Searchs one integer and compares if is the one in the list.

--- karate_chop/php/src/Search.php
+++ karate_chop/php/src/Search.php
@@ -2,7 +2,7 @@
 class Search {
 	
 	public function chop($needle, $number_list) {
-		if (count($number_list) > 0) {
+		if (count($number_list) > 0 && $needle == $number_list[0]) {
 			return 0;
 		}
 		return -1;
