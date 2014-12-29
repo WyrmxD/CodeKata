5699d952c3ab49350272bca247b2aca68be1c102
Iñaki Torres <wyrmxd@gmail.com>
2014-12-29 15:44:33 +0000

    Binary chop refactor: recursive version.

--- karate_chop/php/src/Search.php
+++ karate_chop/php/src/Search.php
@@ -3,22 +3,30 @@ class Search {
 	
 	public function chop($needle, $number_list) {
 
-		if(count($number_list) == 0) {
+		$list_size = count($number_list);
+		if ($list_size == 0) {
 			return -1;
 		}
 
-		if (count($number_list) == 1 && $number_list[0] == $needle) {
-			return 0;
-		}
+		return self::binary_chop($needle, 0, $list_size-1, $number_list);
+	}
 
-		if (count($number_list) > 1) {
-			if ($needle == $number_list[0]) {
-				return 0;				
-			} elseif ($needle == $number_list[1]) {
-				return 1;
-			}
+	private function binary_chop($needle, $first, $last, $number_list) {
+
+		if($first > $last) {
+			return -1;
 		}
-		return -1;
+		$middle = round($last - $first / 2, 0, PHP_ROUND_HALF_DOWN);
+
+		if ($number_list[$middle] == $needle){
+			return $middle;
+		} else if ($number_list[$middle] < $needle) {
+			return self::binary_chop($needle, $first, $middle -1 , $number_list);
+		} else if ($number_list[$middle] > $needle) {
+			return self::binary_chop($needle, $middle +1, $last , $number_list);
+		} else {
+			return -1;
+		}
 	}
 
 }
--- karate_chop/php/tests/SearchTest.php
+++ karate_chop/php/tests/SearchTest.php
@@ -7,18 +7,28 @@ class SearchTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals(-1, Search::chop(1, $number_list));
 	}
 
-	public function testChop_list_whit_one_element_success() {
+	public function testChop_list_with_one_element_success() {
 		$number_list = array(1);
 		$this->assertEquals(0, Search::chop(1, $number_list));
 	}
 
-	public function testChop_list_whit_one_element_fails() {
+	public function testChop_list_with_one_element_fails() {
 		$number_list = array(1);
 		$this->assertEquals(-1, Search::chop(2, $number_list));
 	}
 
-	public function testChop_list_whit_two_elements_success() {
+	public function testChop_list_with_two_elements_success() {
 		$number_list = array(1,2);
 		$this->assertEquals(1, Search::chop(2, $number_list));	
+	}
+
+	public function testChop_list_with_two_elements_fails() {
+		$number_list = array(1,2);
+		$this->assertEquals(-1, Search::chop(3, $number_list));	
+	}
+
+	public function testChop_list_with_several_elemets() {
+		$number_list = array(1,2,3,4);
+		$this->assertEquals(3, Search::chop(4, $number_list));		
 	}
 }
